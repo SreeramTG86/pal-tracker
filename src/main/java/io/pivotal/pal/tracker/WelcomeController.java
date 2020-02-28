c489dde5b22a51c0fa7e7e5ede40162a76adcd37
T G <512747@cognizant.com>
2020-02-28 15:55:48 +0530

    lab three

--- src/main/java/io/pivotal/pal/tracker/WelcomeController.java
+++ src/main/java/io/pivotal/pal/tracker/WelcomeController.java
@@ -2,12 +2,13 @@ package io.pivotal.pal.tracker;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Value;
 
 @RestController
 public class WelcomeController {
 
     @GetMapping("/")
-    public String sayHello() {
-        return "hello";
+    public String sayHello(@Value("${welcome.message}") String message) {
+        return message;
     }
 }
